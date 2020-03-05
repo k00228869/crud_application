5c98d57fe87a6f5015eeb481f17b48a1d111225c
k00228869 <50174686+k00228869@users.noreply.github.com>
2020-03-05 16:17:15 +0000

    displaying and listing assignments
    2 errors on no file for assignments

--- crud-application/src/app/assignments/assignments.component.ts
+++ crud-application/src/app/assignments/assignments.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Assignment } from '../assignment';
+import { ASSIGNMENTS } from '../test-assignments';
+
 @Component({
   selector: 'app-assignments',
   templateUrl: './assignments.component.html',
   styleUrls: ['./assignments.component.css']
 })
 export class AssignmentsComponent implements OnInit {
-
-  assignments = ASSIGNMENTS = {
-    id: 1,
-    name: 'testName',
-    description: 'testDescription'
-  };
+  assignments = ASSIGNMENTS;
+  // assignments = ASSIGNMENTS = {
+  //   id: 1,
+  //   name: 'testName',
+  //   description: 'testDescription'
+  // };
   constructor() { }
 
   ngOnInit(): void {
