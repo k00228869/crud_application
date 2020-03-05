a61e539ca9037a400e1395810de4bf611abe9457
k00228869 <50174686+k00228869@users.noreply.github.com>
2020-03-05 14:21:08 +0000

    adding assignment component

--- crud-application/src/app/app.module.ts
+++ crud-application/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule,
