cff6ea4bc964770d862d40fec4016a3f61254c30
k00228869 <50174686+k00228869@users.noreply.github.com>
2020-03-08 23:31:12 +0000

    function to add and save assignments

--- crud-application/src/app/add-assignment/add-assignment.component.ts
+++ crud-application/src/app/add-assignment/add-assignment.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Assignment } from 'src/assignment';
+import { AssignmentService } from '../assignment.service';
+
 @Component({
   selector: 'app-add-assignment',
   templateUrl: './add-assignment.component.html',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAssignmentComponent implements OnInit {
 
-  constructor() { }
+  assignments: Assignment[];
+  assignmentService: any;
+
+  constructor(assignmentService: AssignmentService) { }
 
   ngOnInit(): void {
   }
 
+  public addNewAssignment(newAssignmentTitle: string, newAssignmentDescription: string)
+{
+    let assignments: Assignment = {id: null, name: newAssignmentTitle, description: newAssignmentDescription};
+
+	  this.assignmentService.addAssignment(assignments).subscribe(   
+      newAssignment => { 
+        console.log(JSON.stringify(newAssignment));
+        this.assignments.push(newAssignment); }
+    );
+}
 }
